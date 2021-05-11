873497727e15b01768c66723a910c32497aa2362
Arnold Olanda <olanda188@gmail.com>
2021-05-11 16:51:28 -0500

    Agregadas configuracion de cabeceras y cors

--- src/index.js
+++ src/index.js
@@ -12,6 +12,14 @@ app.set('json spaces',2);
 //Middleware
 app.use(express.json());
 app.use(morgan('dev'));
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //Routes
 app.get("/", (req, res) =>{
